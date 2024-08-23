46d39f4533bfae89f538b7605dae17168da04107
Nelson <nelson16.code@gmail.com>
2024-08-23 02:54:33 -0400

    chore: add params to docker compose and startup

--- src/startup.sh
+++ src/startup.sh
@@ -15,6 +15,14 @@ then
     php artisan key:generate
 fi
 
+if [ "$1" = "production" ]
+then
+    sed -i -E 's/APP_ENV=.+/APP_ENV=production/g' .env
+    sed -i -E 's/APP_DEBUG=.+/APP_DEBUG=false/g' .env
+    sed -i -E 's/USE_HTTPS=.+/USE_HTTPS=true/g' .env
+    sed -i -E "s/DB_PASSWORD=.+/DB_PASSWORD=$2/g" .env
+fi
+
 if [ "$FIRST_RUN" = true ]
 then
     echo "First time running the app, making migration and seeding."
