337c62b6348be2b6af6f8f8443ce01a50a16c071
Nelson <nelson16.code@gmail.com>
2024-08-22 20:53:55 -0400

    feat: add store and update product endpoint

--- src/app/Http/Controllers/ProductController.php
+++ src/app/Http/Controllers/ProductController.php
@@ -27,7 +27,33 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
-        return $request;
+        try {
+            $request->validate([
+                'name' => 'required|max:50',
+                'description' => 'required|max:200',
+                'price' => 'required|numeric',
+                'expiration_date' => 'required',
+                'image_url' => 'required',
+            ]);
+
+            $product = Product::create([
+                "name" => $request->name,
+                "description" => $request->description,
+                "price" => $request->price,
+                "expiration_date" => $request->expiration_date,
+                "image_url" => $request->image_url,
+            ]);
+
+            return response()->json([
+                'message' => "Success.",
+                'data' => $product,
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+                'data' => [],
+            ], 400);
+        }
     }
 
     public function show(int $id)
@@ -53,7 +79,37 @@ class ProductController extends Controller
 
     public function update(Request $request, int $id)
     {
-        //
+        try {
+            $request->validate([
+                'name' => 'max:50',
+                'description' => 'max:200',
+                'price' => 'numeric',
+            ]);
+
+            $product = Product::find($id);
+
+            if (!$product) {
+                throw new Exception("Product not found.", 404);
+            }
+
+            if ($request->name) $product->name = $request->name;
+            if ($request->description) $product->description = $request->description;
+            if ($request->price) $product->price = $request->price;
+            if ($request->expiration_date) $product->expiration_date = $request->expiration_date;
+            if ($request->image_url) $product->image_url = $request->image_url;
+
+            $product->save();
+
+            return response()->json([
+                'message' => "Success.",
+                'data' => $product,
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => $e->getMessage(),
+                'data' => [],
+            ], 400);
+        }
     }
 
     public function destroy(int $id)
--- src/app/Models/Product.php
+++ src/app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,4 +17,12 @@ class Product extends Model
         'expiration_date',
         'image_url'
     ];
+
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn(int $value) => $value / 100,
+            set: fn(float $value) => intval(($value * 100)),
+        );
+    }
 }
